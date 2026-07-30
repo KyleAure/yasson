bdd2bd350fe8bae9ba42c8858a06398d5de4ecdb
Kyle Aure <KyleJAure@gmail.com>
2026-07-30 16:10:22 -0500

    fix: self review issues

--- src/main/java/org/eclipse/yasson/internal/ClassMultiReleaseExtension.java
+++ src/main/java/org/eclipse/yasson/internal/ClassMultiReleaseExtension.java
@@ -12,19 +12,6 @@
 
 package org.eclipse.yasson.internal;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Method;
-import java.util.Map;
-import java.util.Optional;
-
-import jakarta.json.bind.JsonbException;
-import jakarta.json.bind.config.PropertyNamingStrategy;
-
-import org.eclipse.yasson.internal.model.JsonbCreator;
-import org.eclipse.yasson.internal.model.Property;
-import org.eclipse.yasson.internal.properties.MessageKeys;
-import org.eclipse.yasson.internal.properties.Messages;
-
 /**
  * Search for instance creator from other sources.
  * Mainly intended to add extensibility for different java versions and new features.
